9914169cd4f0b2b10de7e2eaa55e8b4ea78a4ec0
kdd9512 <hyunwoo8805@gmail.com>
2021-07-29 23:34:10 +0900

    React Hooks 06 202100726
    useReducer
    
    ToDos Using React
    
    cleaning...

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -2,15 +2,23 @@ import React from "react";
 import {COMPLETE, DEL, UNCOMPLETED} from "../action";
 import {useDispatch} from "../context";
 
-export default ({text,id,isCompleted}) => {
+export default ({text, id, isCompleted}) => {
     const dispatch = useDispatch();
     return (
         <li>
             <span>{text}</span>
-            <button onClick={() => dispatch({type: DEL, payload: id})}>DELETE</button>
-            <button onClick={() => dispatch({type: isCompleted ? UNCOMPLETED : COMPLETE, payload: id})}>
-                {isCompleted ? "UNCOMPLETED" : "COMPLETE"}
-            </button>
+            {isCompleted ? (
+                <button onClick={() => dispatch({type: isCompleted ? UNCOMPLETED : COMPLETE, payload: id})}>
+                    {isCompleted ? "UNCOMPLETED" : "COMPLETE"}
+                </button>
+            ) : (
+                <>
+                    <button onClick={() => dispatch({type: DEL, payload: id})}>DELETE</button>
+                    <button onClick={() => dispatch({type: isCompleted ? UNCOMPLETED : COMPLETE, payload: id})}>
+                        {isCompleted ? "UNCOMPLETED" : "COMPLETE"}
+                    </button>
+                </>
+            )}
         </li>
     )
 }
