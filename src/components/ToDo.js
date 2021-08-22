0e4023207c9710ce0f90a55a1ceccbdff6a1dc70
kdd9512 <hyunwoo8805@gmail.com>
2021-08-23 01:28:40 +0900

    React Hooks 07 20210823
    useReducer
    
    ToDos Using React
    
    edit 구현

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,26 +1,58 @@
-import React from "react";
+import React, {useState} from "react";
 import {COMPLETE, DEL, EDIT, UNCOMPLETED} from "../action";
-import {useDispatch, useState} from "../context";
+import {useDispatch} from "../context";
 
 export default ({text, id, isCompleted}) => {
-    const [edit, setEdit] = useState(text);
+    const [editText, setEditText] = useState(text);
     const dispatch = useDispatch();
 
-    
+    const onSubmit = (e) => {
+        e.preventDefault();
+        const {target} = e;
+        dispatch({type: EDIT, payload: target[0].value, id});
+        target[0].disabled = true;
+    }
+
+    const editHandler = (e) => {
+        const {target} = e;
+        switch (target.nodeName) {
+            case "BUTTON":
+                const input = target.previousSibling[0];
+                input.disabled = false;
+                input.focus();
+                break;
+            default:
+                return;
+        }
+    }
+
+    const onChange = (e) => {
+        const {
+            target: {value}
+        } = e;
+        setEditText(value);
+    }
 
     return (
         <li>
-            <span>{text}</span>
-            {isCompleted ? (
+            <form onSubmit={onSubmit}>
+                {isCompleted ? (
+                    <input type="text" value={editText} onChange={onChange} disabled/>
+                ) : (
+                    <input type="text" value={editText} onChange={onChange} disabled/>
+                )}
+            </form>
+            {!isCompleted ? (
                 <>
+                    <button onClick={editHandler}>EDIT</button>
+                    <button onClick={() => dispatch({type: DEL, payload: id})}>DELETE</button>
                     <button onClick={() => dispatch({type: isCompleted ? UNCOMPLETED : COMPLETE, payload: id})}>
                         {isCompleted ? "UNCOMPLETED" : "COMPLETE"}
                     </button>
-                    <button onClick={() => dispatch({type: EDIT, payload: text})}>EDIT</button>
                 </>
             ) : (
                 <>
-                    <button onClick={() => dispatch({type: DEL, payload: id})}>DELETE</button>
+                    <button onClick={editHandler}>EDIT</button>
                     <button onClick={() => dispatch({type: isCompleted ? UNCOMPLETED : COMPLETE, payload: id})}>
                         {isCompleted ? "UNCOMPLETED" : "COMPLETE"}
                     </button>
